cc50f321b5f0ba248a03d300fc8bde9aed0bd78f
mine-Proxy <97606543+mine-Proxy@users.noreply.github.com>
2022-08-27 13:56:04 +0800

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -455,7 +455,7 @@ resetpass() {
 
 install() {
     rm /k-install.sh
-    wget https://cdn.jsdelivr.net/gh/kt007007/KTMinerProxy@main/k-install.sh
+    wget https://raw.githubusercontent.com/mine-Proxy/TCMinerProxy/main/KENC/k-install.sh
     chmod 777 /root/k-install.sh
     sudo sh k-install.sh
 }
